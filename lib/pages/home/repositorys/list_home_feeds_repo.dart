dabdd10317e2a374bd7a4155fd977551b212e108
Le Cao The <lecaothe1996@gmail.com>
2022-11-18 17:03:21 +0700

    handle grid image

--- lib/pages/home/repositorys/list_home_feeds_repo.dart
+++ lib/pages/home/repositorys/list_home_feeds_repo.dart
@@ -8,7 +8,7 @@ class ListHomeFeedsRepo {
   Future<List<HomeFeed>> getHomeFeeds() async {
     final res = await _myClient.get(
       '/homefeeds',
-      queryParameters: {'page': '2'},
+      queryParameters: {'page': '1'},
     );
     // print('res====${res.data}');
     if (res.statusCode != 200) {
--- lib/pages/home/widgets/grid_image.dart
+++ lib/pages/home/widgets/grid_image.dart
@@ -1,11 +1,13 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:social_app/pages/home/models/home_feed.dart';
 import 'package:social_app/pages/home/widgets/home_feed_img_item.dart';
 import 'package:social_app/utils/image_utils.dart';
 
 class GridImage extends StatelessWidget {
   final List<Images> images;
+
   // final double padding;
 
   const GridImage({
@@ -34,22 +36,20 @@ class GridImage extends StatelessWidget {
       // case 4:
       // // TODO:
       //   return _buildOneImage(photos[0], width, context);
-      // case 5:
-      // // TODO:
-      //   return _buildOneImage(photos[0], width, context);
+      case 5:
+        return _buildFiveImage(images, width, context);
       default:
         return _buildOneImage(images[0], width, context);
     }
   }
 
   Widget _buildOneImage(Images image, double width, BuildContext context) {
-    // final image = images.i;
     final heightView = ImageUtils.getHeightView(width, image.orgWidth!, image.orgHeight!);
-    final url = ImageUtils.genImgIx(image.url, width.toInt(), heightView.toInt());
+    final urlOneImage = ImageUtils.genImgIx(image.url, width.toInt(), heightView.toInt());
 
-    print('url home feed = $url');
+    print('url One Image = $urlOneImage');
 
-    // view lớn hơn tỷ lệ 16/9
+    // view lớn hơn tỷ lệ 9/16
     if (heightView >= width * 1.8) {
       return GestureDetector(
         onTap: () => navigateToPhotoPage([image], 0, context),
@@ -57,11 +57,8 @@ class GridImage extends StatelessWidget {
           height: width * 1.8,
           width: width,
           child: CachedNetworkImage(
-            imageUrl: url,
+            imageUrl: urlOneImage,
             fit: BoxFit.cover,
-            // placeholder: (context, url) {
-            //   return const Center(child: CircularProgressIndicator());
-            // },
           ),
         ),
       );
@@ -73,78 +70,89 @@ class GridImage extends StatelessWidget {
         height: heightView,
         width: width,
         child: CachedNetworkImage(
-          imageUrl: url,
+          imageUrl: urlOneImage,
           fit: BoxFit.cover,
-          // placeholder: (context, url) {
-          //   return const Center(child: CircularProgressIndicator());
-          // },
         ),
       ),
     );
   }
 
-  Widget _buildTwoImage(List<Images> images, double width, BuildContext context) {
-    final firstImg = images[0];
-
-    // 2 tam cung dung
-    // 2 tam cung ngang
-    // xet dua tren tam dau tien
-    // + 1: tam dau tien dung
-    // + 2: tam dau tien ngang
-
-    //
-
-    if (firstImg.orgWidth! > firstImg.orgHeight!) {
-      final height = width;
-      return SizedBox(
-        height: width,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            HomeFeedImgItem(
-              url: images[0].url ?? '',
-              width: width,
-              height: height / 2,
-              onTap: () => navigateToPhotoPage(images, 0, context),
+  Widget _buildFiveImage(List<Images> images, double width, BuildContext context) {
+    final urlImage1 = ImageUtils.genImgIx(images[0].url, (width- 4) ~/ 2, (width- 4) ~/ 2);
+    final urlImage2 = ImageUtils.genImgIx(images[1].url, (width- 4) ~/ 2, (width - 4) ~/ 3);
+    final urlImage3 = ImageUtils.genImgIx(images[2].url, (width- 4) ~/ 2, (width - 4) ~/ 3);
+    final urlImage4 = ImageUtils.genImgIx(images[3].url, (width- 4) ~/ 2, (width- 4) ~/ 2);
+    final urlImage5 = ImageUtils.genImgIx(images[4].url, (width- 4) ~/ 2, (width - 4) ~/ 3);
+    // print('url Image1 = $urlImage1');
+    // print('url Image2 = $urlImage2');
+    // print('url Image3 = $urlImage3');
+    // print('url Image4 = $urlImage4');
+    // print('url Image5 = $urlImage5');
+    return StaggeredGrid.count(
+      crossAxisCount: 4,
+      mainAxisSpacing: 4,
+      crossAxisSpacing: 4,
+      children: [
+        StaggeredGridTile.count(
+          crossAxisCellCount: 2,
+          mainAxisCellCount: 2,
+          child: GestureDetector(
+            onTap: () => navigateToPhotoPage(images, 0, context),
+            child: CachedNetworkImage(
+              imageUrl: urlImage1,
+              fit: BoxFit.cover,
             ),
-            _buildPadding(),
-            HomeFeedImgItem(
-              url: images[1].url ?? '',
-              width: width,
-              height: height / 2,
-              onTap: () => navigateToPhotoPage(images, 0, context),
+          ),
+        ),
+        StaggeredGridTile.count(
+          crossAxisCellCount: 2,
+          mainAxisCellCount: 4 / 3,
+          child: GestureDetector(
+            onTap: () => navigateToPhotoPage(images, 1, context),
+            child: CachedNetworkImage(
+              imageUrl: urlImage2,
+              fit: BoxFit.cover,
             ),
-          ],
+          ),
         ),
-      );
-    }
-
-    final height = width;
-    width = width;
-    return Row(
-      children: <Widget>[
-        HomeFeedImgItem(
-          url: images[0].url ?? '',
-          width: width / 2,
-          height: height,
-          onTap: () => navigateToPhotoPage(images, 0, context),
+        StaggeredGridTile.count(
+          crossAxisCellCount: 2,
+          mainAxisCellCount: 4 / 3,
+          child: GestureDetector(
+            onTap: () => navigateToPhotoPage(images, 2, context),
+            child: CachedNetworkImage(
+              imageUrl: urlImage3,
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
-        _buildPadding(),
-        HomeFeedImgItem(
-          url: images[1].url ?? '',
-          width: width / 2,
-          height: height,
-          onTap: () => navigateToPhotoPage(images, 0, context),
+        StaggeredGridTile.count(
+          crossAxisCellCount: 2,
+          mainAxisCellCount: 2,
+          child: GestureDetector(
+            onTap: () => navigateToPhotoPage(images, 3, context),
+            child: CachedNetworkImage(
+              imageUrl: urlImage4,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        StaggeredGridTile.count(
+          crossAxisCellCount: 2,
+          mainAxisCellCount: 4 / 3,
+          child: GestureDetector(
+            onTap: () => navigateToPhotoPage(images, 4, context),
+            child: CachedNetworkImage(
+              imageUrl: urlImage5,
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
       ],
     );
   }
 
-  Padding _buildPadding() => Padding(
-    padding: EdgeInsets.only(left: 5, top: 5),
-  );
-
   void navigateToPhotoPage(List<Images> images, int index, BuildContext context) {
-    print('⚡️ Chose image number ${index}');
+    print('⚡️ Chose image number ${index + 1}');
   }
 }
